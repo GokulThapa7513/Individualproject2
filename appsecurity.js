41fa433eac00dc3c1dadcb5338347a4692a88ef6
Thapa Gokul <132889711+GokulThapa7513@users.noreply.github.com>
2023-12-10 04:28:09 +0900

    Add files via upload

---
+++ appsecurity.js
@@ -0,0 +1,21 @@
+function navigatelesson (direction) {
+  if (direction === 'introduction') {
+      window.location.href = 'introduction.html';
+}
+
+else if (direction === 'introduction') {
+      window.location.href = 'design.html';
+}
+
+else if (direction === 'introduction') {
+      window.location.href = 'programmingfundamentals.html';
+}
+
+else if (direction === 'introduction') {
+      window.location.href = 'database.html';
+}
+
+else if (direction === 'introduction') {
+      window.location.href = 'appsecurity.html';
+}
+}
